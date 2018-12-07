0fd60c6d0c474fff7170afff84cef618f4e5b7ea
RAJAT KHEMKA <rkhemka@atrihub.io>
2018-12-07 13:01:35 -0800

    (feat):React_Tutorial
    Learned more about react core concepts by implementing a TODO list applications in React
    
    ICTRAINING-211

---
+++ todo/src/components/todos-list-header.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Todo_List_Header extends Component {
+  state = {};
+  render() {
+    return (
+      <thead>
+        <tr>
+          <th>Task</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+    );
+  }
+}
+
+export default Todo_List_Header;
